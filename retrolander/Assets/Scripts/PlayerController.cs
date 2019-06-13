e3cb8ff6995648dc94ba5ffd71a49188bdc62177
patriciamesttre <patriciamesttre@hotmail.com>
2019-06-12 22:17:38 -0300

    codigo playercontroller iniciado

--- retrolander/Assets/Scripts/PlayerController.cs
+++ retrolander/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+
+
+    public float velocidade = 50f;
+
+    public float combustivel = 1000f;
+
+    public float consumoCombustivel = 1f;
+
+    public float velocidadeLimetePouso = 50f;
+
+    public LayerMask raycastLayerMask;
+
+    public SpriteRenderer propulsorFx;
+
+    public GameObject explosao;
+
+    public Text hudAltitude;
+
+    public Text hudCombustivel;
+
+    public Text hudVelocidadeVertical;
+
+    public Text hudVelocidadeHorizontal;
+
+    private Rigidbody2D rb;
+
+    private float velocidadeVertical;
+
+    private float velocidadeHorizontal;
+
+    private float altitude;
+
+    private string setaEsquerda = "\u2190";
+
+    private string setaDireita = "\u2192";
+
+    private string setaCima = "\u2191";
+
+    private string setaBaixo = "\u2193";
+
+
+
     // Start is called before the first frame update
     void Start()
     {
